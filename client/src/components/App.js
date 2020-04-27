51da2c2a5e397706e5a9bde45cfcee6d3a20df8e
GiorgiKhunashvili <giorgi.khunashvili.1@btu.edu.ge>
2020-04-28 00:27:54 +0400

    added privateroute

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, Route } from 'react-router-dom';
+import { Router, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import CreateSad from './sadgegrdzelo/CreateSad';
@@ -13,13 +13,9 @@ import Signup from './auth/SignUp';
 import Header from './Header';
 import history from '../history'
 
+import PrivateRoute from './PrivateRout';
 class App extends React.Component {
-    requireAuth() {
-        if (this.props.auth.isSignIn === null) {
-            console.log("alooo")
-            history.push('/login')
-        }
-    }
+
     render() {
         return (
             <div className="ui container">
@@ -27,11 +23,12 @@ class App extends React.Component {
                     <div>
                         <Header />
                         <Route path="/" exact component={SadList} />
-                        <Route path="/sad/create" exact component={CreateSad} onClick={this.requireAuth} />
+                        {/* <Route path="/sad/create" exact component={CreateSad} onClick={this.requireAuth} /> */}
                         <Route path="/sad/edit" exact component={EditSad} />
                         <Route path="/sad/delete" exact component={DeleteSad} />
                         <Route path="/login" exact component={Login} />
                         <Route path="/signup" exact component={Signup} />
+                        <PrivateRoute path="/sad/create" exact component={CreateSad} />
                     </div>
                 </Router>
 

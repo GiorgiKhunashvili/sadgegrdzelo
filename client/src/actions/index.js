873bcbb813c7f99d4bbbcf3191d9047df791084e
GiorgiKhunashvili <giorgi.khunashvili.1@btu.edu.ge>
2020-05-07 05:43:18 +0400

    changed redux login action

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -13,8 +13,10 @@ export const login = (formValues) => {
         try {
             const response = await axiosInstance.post('/token/obtain/', {...formValues});
             axiosInstance.defaults.headers['Authorization'] = "JWT" + response.data.access;
+
             localStorage.setItem('access_token', response.data.access);
             localStorage.setItem('refresh_token', response.data.refresh);
+
             isSignIn = true;
             dispatch({
                 type: LOGIN,

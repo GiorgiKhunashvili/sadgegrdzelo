b54b9f4ac3e0078fcbcc28d18c29338fb6bb6cbb
GiorgiKhunashvili <giorgi.khunashvili.1@btu.edu.ge>
2020-05-15 09:12:17 +0400

    added render list home page

--- client/src/components/sadgegrdzelo/SadList.js
+++ client/src/components/sadgegrdzelo/SadList.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
+import { getSadgegrdzeloData } from '../../actions';
 
 class SadList extends React.Component {
-    render() {
-        return (
-            <div className="ui large feed">
-                <div className="event">
+
+    componentDidMount() {
+        this.props.getSadgegrdzeloData();
+    }
+    renderSadList(){
+        return this.props.sad.map((sad, index) => {
+            return (
+                <div className="sad" key={index}>
+                    <div className="event">
                     <div className="label">
                     <img src="/images/avatar/small/helen.jpg" alt="avatar" />
                     </div>
@@ -14,11 +21,12 @@ class SadList extends React.Component {
                         4 days ago
                     </div>
                     <div className="summary">
-                        <a href="/#">Helen Troy</a> added <a href="/#">2 new illustrations</a>
+                        <a href="/#">{sad.user_name}</a> added <a href="/#">2 new illustrations</a>
                     </div>
                     <div className="extra images">
-                        <a href="/#"><img src="/images/wireframe/image.png" alt="uncu duncu" /></a>
-                        <a href="/#"><img src="/images/wireframe/image.png" alt="uncu duncu" /></a>
+                        {/* <a href="/#"><img src="/images/wireframe/image.png" alt="uncu duncu" /></a>
+                        <a href="/#"><img src="/images/wireframe/image.png" alt="uncu duncu" /></a> */}
+                        <audio src={sad.audio_url} controls="controls" />
                     </div>
                     <div className="meta">
                         <a href="/#" className="like">
@@ -26,10 +34,26 @@ class SadList extends React.Component {
                         </a>
                     </div>
                     </div>
+                    </div>
                 </div>
+            )
+        })
+
+    }
+    render() {
+        console.log(this.props)
+        return (
+            <div className="ui large feed">
+                {this.renderSadList()}
             </div>
         )
     }
 }
 
-export default SadList;
+const mapStateToProps = (state) => {
+    return {
+        sad: Object.values(state.sad.allSad) 
+    }
+}
+
+export default connect(mapStateToProps, { getSadgegrdzeloData })(SadList);
